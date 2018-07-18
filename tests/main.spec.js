7951d4a3838a9d6baa02c2a3bba0d58befc66b38
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-18 02:17:31 -0300

    Aula 91. Instalando Sinon e Dependencias

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,26 +1,31 @@
 import { expect } from 'chai';
-import {search, searchAlbums, searchArtists, searchPlaylists } from '../src/main'
+import { search, searchAlbums, searchArtists, searchPlaylists } from '../src/main'
 
 describe('Spotify Wrapper', () => {
-  describe('Smoke tests',() => {
+  describe('Smoke tests', () => {
     // search (genérico) +1 de um tipo
     // searchAlbums
     // searchArtists
     // searchPlaylists
-    it('should exist the search method',()=>{
+    it('should exist the search method', () => {
       expect(search).to.exist;
     });
 
-    it('should exist the searchAlbums method',()=>{
+    it('should exist the searchAlbums method', () => {
       expect(searchAlbums).to.exist;
     });
 
-    it('should exist the searchArtists method',()=>{
+    it('should exist the searchArtists method', () => {
       expect(searchArtists).to.exist;
     });
 
-    it('should exist the searchPlaylists method',()=>{
+    it('should exist the searchPlaylists method', () => {
       expect(searchPlaylists).to.exist;
     });
+  });
+    describe('Generic Search', () => {
+      it('should call fectch function',() => {
+        const artists = search();
+      });
   });
 });
