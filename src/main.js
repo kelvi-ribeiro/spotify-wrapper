68661ccd0c49b507ac3dd504b8b82ecf3df2da3f
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-18 02:09:11 -0300

    Aula 90. Criando Smoke tests

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-const FizzBuzz = (num) =>{
-  if(num === 0) return 0;
-  if(num % 5 === 0 && num % 3 ===0) return 'FizzBuzz';
-  if(num % 3 === 0) return 'Fizz';
-  if(num % 5 === 0) return 'Buzz';
-  return num;
-};
-
-export default FizzBuzz;
+export const search = () => {}
+export const searchAlbums = () => {}
+export const searchArtists = () => {}
+export const searchPlaylists = () => {}
