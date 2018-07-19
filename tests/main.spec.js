276f008aa5972ce7440b3bf48eb1e3433e2cf997
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-19 02:18:00 -0300

    Aula 93. Utilizando o calledWith

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -41,7 +41,19 @@ describe('Spotify Wrapper', () => {
         const fetchedStub = sinon.stub(global,'fetch');
         const artists = search();
 
+        fetchedStub.restore();
+
         expect(fetchedStub).to.have.been.calledOnce;
+      });
+      it('should receive the correct url to fetch',()=>{
+        const fetchedStub = sinon.stub(global,'fetch');
+        const artists = search('Incubus','artist');
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+          const albums = search('Incubus','album');
+
+          expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
       });
   });
 });
