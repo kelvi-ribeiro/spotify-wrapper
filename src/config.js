64aacee3d8859dc92c46f92649e9fc73b1959973
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-25 02:41:56 -0300

    Aula 107. Adicionando Token para autorizacao

--- src/config.js
+++ src/config.js
@@ -1,2 +1,11 @@
-const API_URL = 'https://api.spotify.com/v1';
-export default API_URL;
+const TOKEN_API = '1c036a2f69fe4f3c9485b69b1941beda'
+export const API_URL = 'https://api.spotify.com/v1';
+
+
+export const HEADERS = {
+  headers:{
+    Authorization:`Bearer ${TOKEN_API}`
+  }
+}
+
+
