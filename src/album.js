192e82995b7da033f18ae6fd9b9b5e6218e3b732
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-27 03:51:16 -0300

    Aula 111. Refatorando os metodos de search

--- src/album.js
+++ src/album.js
@@ -1,7 +1,13 @@
-export default function album() {
-  return {
-    getAlbum: id => this.request(`${this.apiURL}/albums/${id}`),
-    getAlbums: ids => this.request(`${this.apiURL}/albums/?ids=${ids}`),
-    getTracks: id => this.request(`${this.apiURL}/albums/${id}/tracks`),
-  };
-}
+/* to run: babel-node albums.js */
+
+global.fetch = require('node-fetch');
+
+import SpotifyWrapper from '../src/index';
+
+const spotify = new SpotifyWrapper({
+  token: '1c036a2f69fe4f3c9485b69b1941beda'
+});
+
+const albums = spotify.search.albums('Incubus');
+
+albums.then(data => data.albums.items.map(item => console.log(item.name)));
